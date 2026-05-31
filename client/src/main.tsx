3700599d52efa5019cbd8f190a6f53e95259f650
Ashutosh Dev Pro <akumar1362004@gmail.com>
2026-05-31 13:11:39 +0530

    Use hash routing for GitHub Pages and rebuild docs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
 const basePath = import.meta.env.BASE_URL?.replace(/\/$/, "") || "/";
 
 createRoot(document.getElementById("root")!).render(
-  <Router base={basePath}>
+  <Router hook={useHashLocation} base={basePath}>
     <App />
   </Router>,
 );
